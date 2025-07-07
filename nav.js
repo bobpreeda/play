0a24bef834dfb0cd2f115677e2a037c5f9dc0d51
Preeda Iramaneerat <preeda@gmail.com>
2025-07-07 11:23:31 +0700

    testing my homepage from cs50

---
+++ nav.js
@@ -0,0 +1,15 @@
+var html = '<nav class="navbar navbar-inverse navbar-fixed-top">\
+                <div class="container-fluid">\
+                    <div class="navbar-header">\
+                        <a class="navbar-brand" href="#"><img src="./Dilbert.png" alt="Bootstrap" width="45" height="36"></a>\
+                    </div>\
+                    <ul class="nav navbar-nav me-auto mb-2 mb-lg-0">\
+                        <li class="active"><a href="./index.html">My Homepage</a></li>\
+                        <li><a href="./bio.html">My Bio</a></li>\
+                        <li><a href="./hobby.html">My Hobby</a></li>\
+                        <li><a href="./contact.html">My Contact</a></li>\
+                    </ul>\
+                </div>\
+            </nav>';
+
+document.getElementById('nav').innerHTML = html;
